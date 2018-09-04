fd5223e4d3d502e6f3ecb270bb701b394b483979
tfwio <tfwroble@gmail.com>
2018-09-03 23:14:09 -0500

    Logger: print to stderr by default and add `Logger.Error(...)` method.

--- .src-lib/cor3/Logger.cs
+++ .src-lib/cor3/Logger.cs
@@ -3,7 +3,6 @@ using System.Diagnostics;
 
 namespace System
 {
-
 	static public class Logger
 	{
 		static public ConsoleColor DefaultForeground = ConsoleColor.White;
@@ -11,33 +10,53 @@ namespace System
 		[Conditional("CONSOLE")]
 		static public void LogY(string title, string filter, params object[] message)
 		{
-			Log(MessageType.Yellow,title,filter,message);
+      Error(MessageType.Yellow,title,filter,message);
 		}
 		[Conditional("CONSOLE")]
 		static public void LogC(string title, string filter, params object[] message)
 		{
-			Log(MessageType.Cyan,title,filter,message);
+      Error(MessageType.Cyan,title,filter,message);
 		}
 		[Conditional("CONSOLE")]
 		static public void LogG(string title, string filter, params object[] message)
 		{
-			Log(MessageType.Green,title,filter,message);
+      Error(MessageType.Green,title,filter,message);
 		}
 		[Conditional("CONSOLE")]
 		static public void LogM(string title, string filter, params object[] message)
 		{
-			Log(MessageType.Magenta,title,filter,message);
+      Error(MessageType.Magenta,title,filter,message);
 		}
 		[Conditional("CONSOLE")]
 		static public void Warn(string title, string filter, params object[] message)
 		{
-			Log(MessageType.Error,title,filter,message);
+      Error(MessageType.Error,title,filter,message);
 		}
+
+    /// <summary>
+    /// Write to stderr.  
+    /// Note that (object) type can be `ConsoleColor`, `MessageType` or `logT` (all the same).
+    /// </summary>
+    [Conditional("CONSOLE")]
+    static public void Error(object type, string title, string filter, params object[] message)
+    {
+      ConsoleColor cc = Console.ForegroundColor;
+      Console.ForegroundColor = (ConsoleColor)type;
+      Console.Error.Write("{0}: ",title);
+      Console.ForegroundColor = DefaultForeground;
+      Console.Error.Write(filter,message);
+      Console.Error.Write("\n");
+      Console.ForegroundColor = cc;
+    }
+    /// <summary>
+    /// Write to stderr.  
+    /// Note that (object) type can be `ConsoleColor`, `MessageType` or `logT` (all the same).
+    /// </summary>
 		[Conditional("CONSOLE")]
 		static public void Log(object type, string title, string filter, params object[] message)
 		{
 			ConsoleColor cc = Console.ForegroundColor;
-			Console.ForegroundColor = (ConsoleColor)type;
+      Console.ForegroundColor = (ConsoleColor)type;
 			Console.Write("{0}: ",title);
 			Console.ForegroundColor = DefaultForeground;
 			Console.Write(filter,message);
